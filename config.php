3841093d95e31e0b68761d34df666cbaf95741ee
RobertaQPC <programmer@quaropianocomunicazione.it>
2022-04-08 11:36:02 +0200

    messaggio

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define("API_BASEURL", "https://ristorantescudiero.localhost:8887/api/");
 define('QPC_DELIVERY_BASEURL', 'https://ristorantescudiero.localhost:8887/storage/app/public/');
 define('QPC_DELIVERY_URL', 'https://ristorantescudiero.localhost:8887'); 
-//test rob untracked
+//test rob untracked --
 //define("API_BASEURL", "https://ristorantescudiero.finedelivery.it/api/");
 //define('QPC_DELIVERY_BASEURL', 'https://ristorantescudiero.finedelivery.it/storage/app/public/');
 //define('QPC_DELIVERY_URL', 'https://ristorantescudiero.finedelivery.it');
