b6c6351e1a086d14d90bd61be9310fa6e207c06a
RobertaQPC <programmer@quaropianocomunicazione.it>
2022-04-26 11:51:32 +0200

    messaggio

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 
 //test rob untracked 
-define("API_BASEURL", "https://ristorantescudiero.finedelivery.it/api/");
-define('QPC_DELIVERY_BASEURL', 'https://ristorantescudiero.finedelivery.it/storage/app/public/');
-define('QPC_DELIVERY_URL', 'https://ristorantescudiero.finedelivery.it');
+define("API_BASEURL", "https://osteriadelviandante.finedelivery.it/api/");
+define('QPC_DELIVERY_BASEURL', 'https://osteriadelviandante.finedelivery.it/storage/app/public/');
+define('QPC_DELIVERY_URL', 'https://osteriadelviandante.finedelivery.it');
 //define('QPC_DELIVERY_BASEURL', 'https://qpc.qpcdev.it/storage/app/public/');
 //define('QPC_DELIVERY_URL', 'https://qpc.qpcdev.it/');
  //define('API_BASEURL', "https://qpc.qpcdev.it/api/");
